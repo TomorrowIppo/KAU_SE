f280f5f3cdc830deaaac36a4e0a3bc5c712f54a6
TomorrowIppo <bjk1624@gmail.com>
2026-04-02 11:43:38 +0900

    fix: main.py 주석 수정

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 class MiniDrive:
     def __init__(self):
-        # [품질: 가시성] 시스템 초기화 상태 로그 출력 [cite: 338]
+        # [품질: 가시성] 시스템 초기화 상태 로그 출력
         self.storage = {} 
         pass
 
     def upload_file(self, user, file_name, content):
-        # TODO: 입력 데이터 유효성 검사 및 예외 처리 로직 구현 [cite: 270]
-        # TODO: 파일 버전 관리(Version Management) 리스트 추가 [cite: 783]
+        # TODO: 입력 데이터 유효성 검사 및 예외 처리 로직 구현
+        # TODO: 파일 버전 관리(Version Management) 리스트 추가
         pass
 
     def list_versions(self, file_name):
-        # TODO: 해당 파일의 버전 메타데이터(시간, 작성자) 출력 로직 구현 [cite: 750]
+        # TODO: 해당 파일의 버전 메타데이터(시간, 작성자) 출력 로직 구현
         pass
 
     def search_file(self, query):
-        # TODO: 파일명 및 속성 기반 검색 필터링 구현 [cite: 775]
+        # TODO: 파일명 및 속성 기반 검색 필터링 구현
         pass
 
 if __name__ == "__main__":
